c3c7440cd9c4c45260a990f065d449462f7d17f2
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-12-13 06:24:26 +0500

    fix

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,13 @@
 import Posts from '@/components/Posts';
+import Suggested from '@/components/Suggested';
 
 export default function Home() {
   return (
     <div className="w-full flex items-center flex-col gap-3 py-18">
-      <Posts />
+      <div className='flex gap-5'>
+        <Posts />
+        <Suggested />
+      </div>
     </div>
   );
 }
