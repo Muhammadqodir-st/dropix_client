aaae277167fbd0552dd58b9bdd8f22b652da7b98
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-12-29 08:56:36 +0500

    some fix

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,27 +1,37 @@
 "use client"
 
+// redux
 import { RootState } from "@/lib/store"
-import Image from "next/image"
 import { useSelector } from "react-redux"
-import { Edit3, UserPen } from "lucide-react"
+
+// next
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+// lucide
+import { Bookmark, Edit3, PanelsTopLeft, UserPen } from "lucide-react"
+
+// components
 import MyPosts from "@/components/MyPosts"
+
 
 export default function Profile() {
 
     // user
     const user = useSelector((state: RootState) => state.user.data)
 
+    // router
+    const router = useRouter()
+
 
     return (
         <div className="max-w-4xl mx-auto p-6">
 
             <div className="flex gap-6 items-center">
-
                 {/* image */}
                 <div className="relative w-32 h-32">
                     <Image src={user ? user.avatar : "/avatar.png"} alt={user ? user.name : 'username'} fill className="rounded-full object-cover" />
                 </div>
-
 
                 <div className="flex-1">
                     <div className="flex items-center gap-4">
@@ -32,13 +42,11 @@ export default function Profile() {
                         </button>
                     </div>
 
-
                     <div className="flex gap-6 mt-3 text-sm">
                         <p><span className="font-semibold">12</span> posts</p>
                         <p><span className="font-semibold">120</span> followers</p>
                         <p><span className="font-semibold">80</span> following</p>
                     </div>
-
 
                     <p className="mt-3 text-sm text-gray-700 max-w-md">
                         {user?.bio || "This user has no bio yet."}
@@ -46,8 +54,14 @@ export default function Profile() {
                 </div>
             </div>
 
-            {/* Divider */}
-            <div className="border-t mt-8"></div>
+            <div className="flex items-center justify-center gap-25 mt-8">
+                <button className="flex items-center gap-2 font-semibold p-3 border-b-2 border-transparent hover:border-white cursor-pointer"><PanelsTopLeft size={20} />Posts</button>
+                <button className="flex items-center gap-2 font-semibold p-3 border-b-2 border-transparent hover:border-white cursor-pointer"><Bookmark size={20} />Saved</button>
+                <button className="flex items-center gap-2 font-semibold p-3 border-b-2 border-transparent hover:border-white cursor-pointer"><PanelsTopLeft size={20} />Posts</button>
+            </div>
+
+
+            <div className="border-t"></div>
 
             {/* Posts */}
             <div className="mt-6">
