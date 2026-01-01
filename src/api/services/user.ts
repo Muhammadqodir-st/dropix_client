79b5ea82757122fa2b7001e496ebf7cd8869bbc7
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-01 21:12:15 +0500

    some fix

--- src/api/services/user.ts
+++ src/api/services/user.ts
@@ -1,10 +1,30 @@
-import { fetcher } from "../fetcher";
+import { fetcher, getToken } from "../fetcher";
 
+// get by id
 export function getByIdUser({ id }: { id: string }) {
     return fetcher(`/user/${id}`, {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
+        }
+    })
+}
+
+// update user
+export function updateUser(data: { file?: File | null, name?: string, bio?: string }) {
+    const token = getToken()
+
+    const formData = new FormData()
+
+    if (data.file) formData.append('file', data.file)
+    if (data.name) formData.append('name', data.name)
+    if (data.bio) formData.append('bio', data.bio)
+
+    return fetcher('/user/update', {
+        method: "POST",
+        body: formData,
+        headers: {
+            'Authorization': `Bearer ${token}`
         }
     })
 }
--- src/app/(root)/settings/edit/page.tsx
+++ src/app/(root)/settings/edit/page.tsx
@@ -2,6 +2,7 @@
 
 // tanstack
 import { useForm } from "@tanstack/react-form"
+import { useMutation } from "@tanstack/react-query";
 
 // redux
 import { RootState } from "@/lib/store";
@@ -10,6 +11,13 @@ import { useSelector } from "react-redux";
 // next
 import Image from "next/image";
 
+// api service
+import { updateUser } from "@/api/services/user";
+
+// components
+import ButtonLoader from "@/components/loaders/ButtonLoader";
+
+
 // IForm
 interface IForm {
     file: null | File;
@@ -17,11 +25,17 @@ interface IForm {
     bio: string;
 }
 
-
 export default function page() {
 
     // user
     const user = useSelector((state: RootState) => state.user.data)
+
+    // mutation
+    const updateMutation = useMutation({
+        mutationFn: async ({ file, name, bio }: IForm) => {
+            return await updateUser({ file, name, bio })
+        },
+    })
 
     // use form
     const form = useForm({
@@ -29,7 +43,11 @@ export default function page() {
             file: null,
             name: '',
             bio: ''
-        } as IForm
+        } as IForm,
+        onSubmit: ({ value }) => {
+            updateMutation.mutateAsync(value)
+            form.reset()
+        }
     })
 
 
@@ -41,13 +59,13 @@ export default function page() {
             </div>
 
             {/* body */}
-            <form onSubmit={(e) => e.preventDefault()} className="flex flex-col gap-8 items-end">
+            <form onSubmit={(e) => { e.preventDefault(); form.handleSubmit() }} className="flex flex-col gap-8 items-end">
                 {/* image */}
                 <form.Field name="file">
                     {(field) => (
-                        <div className="w-full border flex items-center justify-between p-3 rounded-lg">
+                        <div className="w-full border flex items-center justify-between p-4 rounded-lg">
                             <input className="hidden" type="file" id="profileImage" onChange={(e) => field.handleChange(e.target.files ? e.target.files[0] : null)} />
-                            <Image className="w-18 h-18 rounded-full object-cover" src={field.state.value ? URL.createObjectURL(field.state.value) : user ? user.avatar : '/assets/defualt-user.jpg'} alt={user ? user.name : 'username'} width={100} height={100} />
+                            <Image className="w-16 h-16 rounded-full object-cover" src={field.state.value ? URL.createObjectURL(field.state.value) : user ? user.avatar : '/assets/defualt-user.jpg'} alt={user ? user.name : 'username'} width={100} height={100} />
                             <label htmlFor="profileImage" className="py-2 px-3 bg-blue-700 rounded-lg text-sm font-semibold cursor-pointer">change photo</label>
                         </div>
                     )}
@@ -58,7 +76,7 @@ export default function page() {
                     {(filed) => (
                         <label className="w-full flex flex-col gap-1" htmlFor="nameInput">
                             <p className="text-sm font-semibold">Name</p>
-                            <input className="w-full border py-3 px-3 rounded-lg outline-0" type="text" value={filed.state.value} onChange={(e) => filed.handleChange(e.target.value)} id="nameInput" />
+                            <input className="w-full border py-3 px-3 rounded-lg focus:outline outline-white`" type="text" value={filed.state.value} onChange={(e) => filed.handleChange(e.target.value)} id="nameInput" placeholder={user ? user.name + '...' : 'name ....'} />
                         </label>
                     )}
                 </form.Field>
@@ -68,13 +86,15 @@ export default function page() {
                     {(field) => (
                         <label className="w-full flex flex-col gap-1" htmlFor="">
                             <p className="text-sm font-semibold">Boi</p>
-                            <textarea value={field.state.value} onChange={(e) => field.handleChange(e.target.value)} className="w-full h-30 border py-2 px-3 rounded-lg outline-0" maxLength={150}></textarea>
+                            <textarea value={field.state.value} onChange={(e) => field.handleChange(e.target.value)} className="w-full h-30 border py-2 px-3 rounded-lg focus:outline outline-white" maxLength={150} placeholder={user ? user.bio : 'bio...'}></textarea>
                             <p className="text-sm text-end">{field.state.value?.length}/150</p>
-                        </label>
+                        </label>    
                     )}
                 </form.Field>
 
-                <button className="py-2 px-4 bg-blue-700 rounded-lg text-sm font-semibold cursor-pointer">update profile</button>
+                <button className={`py-2 px-4  rounded-lg text-sm font-semibold cursor-pointer`}>
+                    {updateMutation.isPending ? <ButtonLoader /> : 'update profile'}
+                </button>
             </form>
         </div>
     )
